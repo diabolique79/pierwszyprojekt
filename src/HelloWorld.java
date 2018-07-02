a98dbef5728f7205aa885801917208dce6fab0fc
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-02 18:53:51 +0200

    Dodanie pliku HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Run Forest, run");
+        System.out.println("Run Forest1, run");
+        System.out.println("Run Forest2, run");
+        System.out.println("Run Forest3, run");
+        System.out.println("Run Forest4, run");
+        System.out.println("Run Forest5, run");
+    }
+    }
+
+
